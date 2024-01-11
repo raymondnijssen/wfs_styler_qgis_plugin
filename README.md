90da476a55db76eb6e0f3e8fb2b93f975f193ad6
Raymond Nijssen <r.nijssen@terglobo.nl>
2024-01-11 12:56:08 +0100

    Set EUPL

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wfs_styler_qgis_plugin
 QGIS plugin for setting a WMS style to a WFS layer
+
+
+## Licence
+
+This software is licenced under the EUPL.
+[]
--- wfs_styler/wfs_probe.py
+++ wfs_styler/wfs_probe.py
@@ -1,15 +1,8 @@
 # ---------------------------------------------------------------------
-# Copyright (C) 2022 Raymond Nijssen
+# Copyright (C) 2023 Raymond Nijssen
 # ---------------------------------------------------------------------
-# Licensed under the terms of GNU GPL 2
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# Licensed under the EUPL
 # ---------------------------------------------------------------------
-
-#import os
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtNetwork import QNetworkRequest
